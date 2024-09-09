5851d164258ef00941c4fddb3a1ec881e4df7329
blosttt <111206726+blosttt@users.noreply.github.com>
2024-09-09 00:59:10 -0300

    Configuracion del Menu y reacion del home

--- Parking Check/src/App.tsx
+++ Parking Check/src/App.tsx
@@ -7,19 +7,27 @@ import {
   IonTabBar,
   IonTabButton,
   IonTabs,
+  IonMenu,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonList,
+  IonItem,
+  IonButtons,
+  IonMenuButton,
   setupIonicReact
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import { ellipse, square, triangle,person } from 'ionicons/icons';
+import { ellipse, square, triangle, person, homeOutline, alertCircleOutline } from 'ionicons/icons';
 
 /*paginas */ 
 import Tab1 from './pages/Tab1';
 import Tab2 from './pages/Tab2';
 import Profile from './pages/profile';
-
 import LoginRegister from './pages/LoginRegister/LoginRegister';
-
 import Invited from './pages/invited/invited';
+import Home from './pages/Home';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -37,17 +45,6 @@ import '@ionic/react/css/text-transformation.css';
 import '@ionic/react/css/flex-utils.css';
 import '@ionic/react/css/display.css';
 
-/**
- * Ionic Dark Mode
- * -----------------------------------------------------
- * For more info, please see:
- * https://ionicframework.com/docs/theming/dark-mode
- */
-
-/* import '@ionic/react/css/palettes/dark.always.css'; */
-/* import '@ionic/react/css/palettes/dark.class.css'; */
-import '@ionic/react/css/palettes/dark.system.css';
-
 /* Theme variables */
 import './theme/variables.css';
 
@@ -56,49 +53,77 @@ setupIonicReact();
 const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
-      <IonTabs>
-        <IonRouterOutlet>
-          
-          <Route exact path="/login">
-            <LoginRegister />
-          </Route>
-          <Route exact path="/invited">
-            <Invited />
-          </Route>
-          
+      {/* Menú lateral */}
+      <IonMenu side="end" contentId="main">
+        <IonHeader>
+          <IonToolbar color="primary">
+            <IonTitle>Parking Check</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent>
+          <IonList>
+            <IonItem routerLink="/tab1">
+              <IonIcon icon={homeOutline} slot="start" />
+              <IonLabel>Asignación Estacionamiento</IonLabel>
+            </IonItem>
+            <IonItem routerLink="/profile">
+              <IonIcon icon={person} slot="start" />
+              <IonLabel>Mi Perfil</IonLabel>
+            </IonItem>
+            <IonItem routerLink="/tab2">
+              <IonIcon icon={alertCircleOutline} slot="start" />
+              <IonLabel>Reportar un problema</IonLabel>
+            </IonItem>
+          </IonList>
+        </IonContent>
+      </IonMenu>
 
-          <Route exact path="/tab1">
-            <Tab1 />
-          </Route>
-          <Route exact path="/tab2">
-            <Tab2 />
-          </Route>
-          <Route path="/profile">
-            <Profile />
-          </Route>
-          <Route exact path="/">
-            <Redirect to="/login" />
-          </Route>
-        </IonRouterOutlet>
-        <IonTabBar slot="bottom">
+      {/* Contenido principal y Tabs */}
+      <IonRouterOutlet id="main">
+        <IonTabs>
+          <IonRouterOutlet>
+            <Route exact path="/login">
+              <LoginRegister />
+            </Route>
+            <Route exact path="/invited">
+              <Invited />
+            </Route>
 
-          <IonTabButton tab="login" href="/login">
-            <IonIcon aria-hidden="true" icon={triangle} />
-            <IonLabel>login</IonLabel>
-          </IonTabButton>
+            <Route exact path="/tab1">
+              <Tab1 />
+            </Route>
+            <Route exact path="/tab2">
+              <Tab2 />
+            </Route>
+            <Route path="/profile">
+              <Profile />
+            </Route>
+            <Route exact path="/login">
+              <Redirect to="/login" />
+            </Route>
+            <Route path ="/home">
+              <Home />
+            </Route>
+          </IonRouterOutlet>
 
-          <IonTabButton tab="SignUp" href="/SignUp">
-            <IonIcon aria-hidden="true" icon={ellipse} />
-            <IonLabel>Register</IonLabel>
-          </IonTabButton>
+          <IonTabBar slot="bottom">
+            <IonTabButton tab="home" href="/home">
+              <IonIcon aria-hidden="true" icon={triangle} />
+              <IonLabel>Home</IonLabel>
+            </IonTabButton>
 
-          <IonTabButton tab="Profile" href="/profile">
-            <IonIcon aria-hidden="true" icon={person} />
-            <IonLabel>Perfil</IonLabel>
+            <IonTabButton tab="SignUp" href="/SignUp">
+              <IonIcon aria-hidden="true" icon={ellipse} />
+              <IonLabel>Register</IonLabel>
+            </IonTabButton>
 
-          </IonTabButton>
-        </IonTabBar>
-      </IonTabs>
+            <IonTabButton tab="Profile" href="/profile">
+              <IonIcon aria-hidden="true" icon={person} />
+              <IonLabel>Perfil</IonLabel>
+            </IonTabButton>
+          </IonTabBar>
+        </IonTabs>
+      </IonRouterOutlet>
     </IonReactRouter>
   </IonApp>
 );
