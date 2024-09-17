0be4a0636425c891e892ea157c81a7aa4d5259ff
ffuentealba19 <ffuentealba2020@alu.uct.cl>
2024-09-17 00:02:52 -0300

    Solución div mal centrado

--- Parking Check/src/pages/invited/invited.tsx
+++ Parking Check/src/pages/invited/invited.tsx
@@ -15,19 +15,21 @@ const Invited: React.FC = () => {
           <IonCardHeader>
             <IonCardTitle className="card-title">INVITADOS</IonCardTitle>
           </IonCardHeader> 
-          <div className="invited-input-wrapper">
-                <IonInput type="text" placeholder="Ingrese su nombre" className="invited-input-field" clearInput={true}  ></IonInput>
-                <IonInput type="tel" placeholder="Ingrese su numero de Telefono" className="invited-input-field" clearInput={true}></IonInput>
-                <IonInput type="text" placeholder="Ingrese su patente" className="invited-input-field" clearInput={true}></IonInput>
-                <IonButton expand="block" className="invited-button">REGISTRARSE</IonButton>
-                <div className="invited-guest-session">
-                  <IonLabel>Ya tienes una cuenta o quieres una? </IonLabel><br></br>
-                  <IonLabel color="primary" className="invited-guest-link" >
-                     <IonRouterLink routerLink='/login'> Inicia sesion o registrate</IonRouterLink>
-                  </IonLabel>
-                </div>
-                <br/>
-          </div>
+          <IonCardContent >
+            <div className="invited-input-wrapper">
+                  <IonInput type="text" placeholder="Ingrese su nombre" className="invited-input-field" clearInput={true}  ></IonInput>
+                  <IonInput type="tel" placeholder="Ingrese su numero de Telefono" className="invited-input-field" clearInput={true}></IonInput>
+                  <IonInput type="text" placeholder="Ingrese su patente" className="invited-input-field" clearInput={true}></IonInput>
+                  <IonButton expand="block" className="invited-button">REGISTER</IonButton>
+                  <div className="invited-guest-session">
+                    <IonLabel>Ya tienes una cuenta o quieres una? </IonLabel><br></br>
+                    <IonLabel color="primary" className="invited-guest-link" >
+                       <IonRouterLink routerLink='/login'> Inicia sesion o registrate</IonRouterLink>
+                    </IonLabel>
+                  </div>
+                  <br/>
+            </div>
+          </IonCardContent>
           </IonCard>
         </IonContent>
       </IonPage>
