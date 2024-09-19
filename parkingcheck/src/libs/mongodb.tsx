20ced3345f7fbf9ef6aef8758b65f269af784f8f
MatiasV4squez <mvasquez2023@alu.uct.cl>
2024-09-19 02:06:40 -0300

    por mientras (esta branch sera eliminada)

--- parkingcheck/src/libs/mongodb.tsx
+++ parkingcheck/src/libs/mongodb.tsx
@@ -1,21 +1,22 @@
 import { MongoClient, ServerApiVersion } from 'mongodb';
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = process.env.CLUSTER || 'mongodb+srv://parker:avocato@parkingcheck.q8rss.mongodb.net/?retryWrites=true&w=majority'
+const uri = process.env.CLUSTER || 'mongodb+srv://parker:avocato@parkingcheck.q8rss.mongodb.net/?retryWrites=true&w=majority';
 
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
+
 export async function run() {
- 
-
+  try {
     await client.connect();
-
-    await client.db("admin").command({ ping: 1 });
-    console.log("Se ha conectado con MongoDB");
-
+    console.log("Conectado a MongoDB");
+    return client;
+  } catch (error) {
+    console.error("Error al conectar con MongoDB", error);
+    throw error;
+  }
 }
