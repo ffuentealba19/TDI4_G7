a4fc1cc4b4d1967f07b227b8204850eda0f97b92
Naiko <nchandia2020@alu.uct.cl>
2024-10-05 15:25:32 -0300

    Solucion al error del proyecto
    borramos los vestigios de los componentes que fuieron quitados en el push anterior para que la aplicacion corra sin problemas

--- Parking Check/src/pages/LoginRegister/LoginRegister.tsx
+++ Parking Check/src/pages/LoginRegister/LoginRegister.tsx
@@ -2,12 +2,8 @@ import React, { useState } from 'react';
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonInput, IonLabel, IonButton, IonSegment, IonSegmentButton, IonRouterLink, IonToast, IonIcon } from '@ionic/react';
 import { logoGoogle } from 'ionicons/icons';
 import './login.css';
-import { auth, provider } from '../../firebase-config';
-import { signInWithPopup } from 'firebase/auth';
 import { registerUser } from '../../api/api';
 import { useHistory } from 'react-router-dom'; // Importa useHistory para redirigir
-import { useAuth } from '../../context/authcontext';
-import { Redirect } from 'react-router-dom';
 
 
 const LoginRegister: React.FC = () => {
@@ -18,11 +14,7 @@ const LoginRegister: React.FC = () => {
   const [toastMessage, setToastMessage] = useState('');
   const [showToast, setShowToast] = useState(false);
   const history = useHistory(); // Inicializa useHistory
-  const { user } = useAuth();  // Obtener el estado de autenticación
 
-  if (user) {
-    return <Redirect to="/home" />;  // Redirigir al home si ya está logueado
-  }
 
   // Función para manejar el registro de usuario
   const handleRegister = async () => {
@@ -37,35 +29,6 @@ const LoginRegister: React.FC = () => {
     }
   };
 
-  // Función para iniciar sesión con Google
-  const handleGoogleLogin = async () => {
-    try {
-      const result = await signInWithPopup(auth, provider);
-      console.log('User:', result.user);
-      setToastMessage('Inicio de sesión exitoso con Google');
-      setShowToast(true);
-      history.push('/home'); // Redirigir al Home después del login
-    } catch (error) {
-      console.error('Error:', error);
-      setToastMessage('Error al iniciar sesión con Google');
-      setShowToast(true);
-    }
-  };
-
-  // Función para registrarse con Google
-  const handleGoogleRegister = async () => {
-    try {
-      const result = await signInWithPopup(auth, provider);
-      console.log('User:', result.user);
-      setToastMessage('Registro exitoso con Google');
-      setShowToast(true);
-      history.push('/home'); // Redirigir al Home después del registro con Google
-    } catch (error) {
-      console.error('Error:', error);
-      setToastMessage('Error al registrarse con Google');
-      setShowToast(true);
-    }
-  };
 
   return (
     <IonPage>
@@ -102,7 +65,7 @@ const LoginRegister: React.FC = () => {
                 <IonButton expand="block" className="login-button">INICIAR SESIÓN</IonButton>
                 {/* label de separacion */}
                 <IonLabel className="separator">O</IonLabel>
-                <IonButton expand="block" className="google-login-button" onClick={handleGoogleLogin} color="secondary" >
+                <IonButton expand="block" className="google-login-button" onClick={alert} color="secondary" >
                   <IonIcon icon={logoGoogle} slot="start" />
                   Iniciar sesión con Google
                 </IonButton>
