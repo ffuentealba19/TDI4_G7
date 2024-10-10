44feb855113c26a1412d0c5e90395879c4f2e3a7
Naiko <nchandia2020@alu.uct.cl>
2024-10-09 23:34:15 -0300

    configuracion package
    se modifican unas cosas para el deploy

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,6 +10,7 @@ const app = express();
 
 // Conexión a MongoDB usando la variable de entorno
 const uri = process.env.MONGODB_URI;
+
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Error connecting to MongoDB', err));
