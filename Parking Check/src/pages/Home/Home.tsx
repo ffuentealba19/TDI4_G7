d06f60ae87ac5a40c412c69d033836ca1d2c56c1
blost <bsobarzo2019@alu.uct.cl>
2024-10-19 05:50:19 -0300

    Upgrade del Menu

--- Parking Check/src/pages/Home/Home.tsx
+++ Parking Check/src/pages/Home/Home.tsx
@@ -1,4 +1,17 @@
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonMenuButton, IonCard, IonCardTitle, IonCardSubtitle, IonCardHeader, IonCardContent, IonText} from '@ionic/react';
+import { 
+  IonPage, 
+  IonHeader, 
+  IonToolbar, 
+  IonTitle, 
+  IonContent, 
+  IonMenuButton, 
+  IonCard, 
+  IonCardTitle, 
+  IonCardHeader, 
+  IonCardContent, 
+  IonText, 
+  IonButton 
+} from '@ionic/react';
 import './Home.css';
 
 const Home: React.FC = () => {
@@ -6,28 +19,48 @@ const Home: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar color="primary">
-          <IonMenuButton slot="end" /> {/* El botón de menú va aquí */}
+          <IonMenuButton slot="end" /> {/* Botón de menú */}
           <IonTitle>Parking Check</IonTitle>
         </IonToolbar>
       </IonHeader>
 
       <IonContent fullscreen={true}>
-        <IonText><h1>Estacionamientos Disponibles</h1></IonText>
-        <IonCard>
-          <img alt='Campus Norte' src='https://doctoradoeduconsorcio.cl/wp-content/uploads/catolica-temuco.jpg'/>
+
+        {/* Título principal */}
+        <IonText>
+          <h1 className="page-title">Estacionamientos Disponibles</h1>
+        </IonText>
+
+        {/* Card para Campus Norte */}
+        <IonCard className="card">
+          <img alt='Campus Norte' src='https://doctoradoeduconsorcio.cl/wp-content/uploads/catolica-temuco.jpg' className="card-image" />
           <IonCardHeader>
             <IonCardTitle>Campus Norte</IonCardTitle>
           </IonCardHeader>
-          <IonCardContent>Disponible: 90/150</IonCardContent>
+          <IonCardContent>
+            Disponible: 90/150
+            {/* Botón para reservar */}
+            <IonButton expand="block" color="success" className="reserve-button">
+              Reservar
+            </IonButton>
+          </IonCardContent>
         </IonCard>
-        <IonCard>
-          <img alt='Campus Sur' className="grayscale-img" src='https://www.uct.cl/content/uploads/san-francisco-3.jpg'/>
+
+        {/* Card para San Francisco (próximamente) */}
+        <IonCard className="card upcoming">
+          <img alt='Campus Sur' className="card-image bw-image" src='https://www.uct.cl/content/uploads/san-francisco-3.jpg'/>
           <IonCardHeader>
             <IonCardTitle>San Francisco</IonCardTitle>
           </IonCardHeader>
-          <IonCardContent>Proximamente...</IonCardContent>
+          <IonCardContent>
+            Próximamente...
+            {/* Botón deshabilitado para indicar que no está disponible */}
+            <IonButton expand="block" color="medium" disabled className="reserve-button">
+              No Disponible
+            </IonButton>
+          </IonCardContent>
         </IonCard>
-        
+
       </IonContent>
     </IonPage>
   );
