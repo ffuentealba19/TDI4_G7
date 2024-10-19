d8b214de5af4073e2ba4fc9db420e817bddebf94
Pamtom21 <joluis.2.11.04@gmail.com>
2024-10-19 00:49:25 -0300

    Parte de extraer estacionamientos

--- parkingcheck/src/app/api/Get_Parking/route.ts
+++ parkingcheck/src/app/api/Get_Parking/route.ts
@@ -1,9 +1,24 @@
 import Parking from "@/models/parking";
 import { run } from "@/libs/mongodb";
 import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
 
 export async function GET() {
     await run()
-    const All_park = Parking.find
-    
+    const All_park = await Parking.find({
+        section: "A"
+    })
+    const tamano = All_park.length
+    let i: number;
+    const estacionamiento = []
+    for(i=0; i < tamano ; i++)
+    {
+        estacionamiento.push(All_park[i].section+"-"+All_park[i].number)
+    }
+    console.log(estacionamiento)
+    const estacionamientos= {
+        A: estacionamiento
+    }
+    console.log(estacionamientos)
+    return NextResponse.json({message: "Hola"})
 }
