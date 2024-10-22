0176173634baa5633979088e4156af720ddfa130
MatiasV4squez <mvasquez2023@alu.uct.cl>
2024-10-21 21:15:11 -0300

    Se ha terminado el popup para el usuario vip y el no vip

--- parkingcheck/src/app/Home/page.tsx
+++ parkingcheck/src/app/Home/page.tsx
@@ -2,12 +2,15 @@
 
 import { Navbar } from '../components/Navbar';
 import ParkingMap from '../components/ParkingMap';
+import Popup from '../components/Popup'; // Asegúrate de importar el componente Popup
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import jwt from 'jsonwebtoken';
+import Image from 'next/image';
 
 export default function Home() {
     const [isVip, setIsVip] = useState<boolean | null>(null);
+    const [showPopup, setShowPopup] = useState<boolean>(false); // Nuevo estado para controlar el popup
     const router = useRouter();
 
     useEffect(() => {
@@ -20,30 +23,53 @@ export default function Home() {
         const tokenValue = token.split('=')[1];
         try {
             const decodedToken: any = jwt.decode(tokenValue);
-            console.log(decodedToken)
+            console.log(decodedToken);
             if (decodedToken && decodedToken.vip !== undefined) {
                 setIsVip(decodedToken.vip);
+                setShowPopup(true); // Mostrar el popup para ambos tipos de usuarios
             }
         } catch (error) {
             console.error("Error al decodificar el token:", error);
         }
     }, [router]);
 
+    const handleClosePopup = () => {
+        setShowPopup(false); // Cierra el popup
+    };
+
     return ( 
         <div>
             <Navbar></Navbar>
             {isVip ? (
                 <div>
-                    vip
-                    <ParkingMap/>
+                    VIP
+                    <ParkingMap />
                 </div>
             ) : (
                 <div>
-                    no vip
-                    <ParkingMap/>
+                    <ParkingMap />
                 </div>
             )}
 
+            {showPopup && (
+                <Popup onClose={handleClosePopup} title={isVip ? "Bienvenido VIP" : "Bienvenido"}>
+                    {isVip ? (
+                        <Image 
+                        src="/vip.png" 
+                        alt="Papelera" 
+                        width={1280} 
+                        height={720}
+                    />
+                    ) : (
+                        <Image 
+                        src="/no-vip.png" 
+                        alt="Papelera" 
+                        width={1280} 
+                        height={720}
+                    />
+                    )}
+                </Popup>
+            )}
         </div>
     );
-};
+}
