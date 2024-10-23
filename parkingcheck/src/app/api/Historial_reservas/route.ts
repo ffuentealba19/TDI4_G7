374bb405ddc9d79629d7a247864c2d7bc31cc5e6
Pamtom21 <joluis.2.11.04@gmail.com>
2024-10-23 12:58:27 -0300

    Historial de reservas front

--- parkingcheck/src/app/api/Historial_reservas/route.ts
+++ parkingcheck/src/app/api/Historial_reservas/route.ts
@@ -1,20 +1,26 @@
+import { NextResponse } from "next/server";
 import Reserva from "@/models/reserva";
-import User2 from "@/models/users copy";
 import { run } from "@/libs/mongodb";
-import { NextRequest,  NextResponse } from "next/server";
 import { cookies } from "next/headers";
 import { verify } from "jsonwebtoken";
 
 export async function GET() {
+    try {
+        const galleta = cookies();
+        const token = galleta.get("token")?.value;
+        if (!token) {
+            return NextResponse.json({ error: "No autorizado" }, { status: 401 });
+        }
 
-    const galleta = cookies()
-    const token = galleta.get("token")?.value
-    const secret = process.env.JWT_SECRET
-    const user = verify(token, secret) as {userId : string}
-    const id =  user.userId
-    await run()
-    const consulta = await Reserva.find({
-        id_usuario : id
-    })
-    return NextResponse.json({consulta})
+        const secret = process.env.JWT_SECRET;
+        const user = verify(token, secret) as { userId: string };
+        const id = user.userId;
+
+        await run();
+        const reservas = await Reserva.find({ id_usuario: id });
+
+        return NextResponse.json(reservas);
+    } catch (error) {
+        return NextResponse.json({ error: error.message }, { status: 500 });
+    }
 }
