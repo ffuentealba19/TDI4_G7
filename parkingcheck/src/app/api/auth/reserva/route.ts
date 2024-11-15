219ff0c8d70ce8840302010ef20dfac2e2aec99a
Lukas Escobar <lescobar2023@alu.uct.cl>
2024-11-15 17:34:16 -0300

    eliminación de comentarios innecesarios

--- parkingcheck/src/app/api/auth/reserva/route.ts
+++ parkingcheck/src/app/api/auth/reserva/route.ts
@@ -1,15 +1,14 @@
-import apiClient from "@/libs/axios";  // Asegúrate de que apiClient esté configurado correctamente
+import apiClient from "@/libs/axios";
 
 // Función para crear una reserva
 const createReserva = async (seccion:string, numero:string, correo:string, fechaReserva:string, fechaExpiracion:string, userId:string) => {
   try {
-    // Preparar los datos a enviar a la API
+
     const reservaData = { seccion, numero, correo, fechaReserva, fechaExpiracion, userId };
 
-    // Hacer la solicitud POST para crear la reserva
+
     const response = await apiClient.post('/reservas', reservaData);
 
-    // Retornar la respuesta de la API, que contiene los detalles de la reserva creada
     return response.data;
   } catch (error) {
     console.error("Error al crear la reserva:", error);
--- parkingcheck/src/libs/axios.ts
+++ parkingcheck/src/libs/axios.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const apiclient = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'http://localhost:4000',
     headers: {
         'Content-Type': 'application/json',
     },
